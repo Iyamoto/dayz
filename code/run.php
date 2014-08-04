310b7ee0c90cdda497a16a2a2273f9ad29a0a1fc
Iyamoto <Iyamoto@users.noreply.github.com>
2014-08-04 12:18:41 +0400

    names

--- code/run.php
+++ code/run.php
@@ -4,13 +4,13 @@ error_reporting(E_ALL & ~E_USER_NOTICE | E_STRICT);
 
 require_once dirname(__FILE__) . "/../lib/steam-condenser.php";
 
-function getFixture($fileName) {
-    return file_get_contents(dirname(__FILE__) . "/fixtures/$fileName");
-}
-
 $server = new GoldSrcServer('173.199.67.130', 27017);
 $server->initialize();
-var_dump($server->getPlayers());
+$players = $server->getPlayers();
+foreach($players as $player){
+	//var_dump($player);
+	echo $player->getName() ."\n";
+}
 
 
 //http://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr=173.199.67.130
