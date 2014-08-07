781fc176a7a77d01ec3ff756b93e5089ac3d89f3
Iyamoto <Iyamoto@users.noreply.github.com>
2014-08-07 15:53:41 +0400

    testing sqlite

--- list.php
+++ list.php
@@ -18,10 +18,10 @@ if (!$con) {
 //Get blacklist db
 
 if ($db = new SQLite3("base.db")) { 
-    $result = sqlite_query($db, 'select id, steamid, status from users');
-    var_dump(sqlite_fetch_array($result)); 
+    $results = $db->query('select id, steamid, status from users');
+    var_dump($results->fetchArray()); 
 } else {
-    die($sqliteerror);
+    die('black list not found');
 }
 		
 //Form table
